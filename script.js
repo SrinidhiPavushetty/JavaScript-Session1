06dbbc271340ae317b28135429e47e153b02cc57
SrinidhiPavushetty <pavushettysrinidhi@gmail.com>
2022-03-09 19:18:00 +0530

    added all

---
+++ script.js
@@ -0,0 +1,39 @@
+//Create a variable of differnt data types
+console.log("Different types of data types")
+console.log("String")
+console.log("INTEGER")
+console.log("Float")
+console.log("Boolean")
+
+//using console.log to get output
+console.log("Welcome to the course.")
+console.log(5)
+console.log(5.9)
+console.log(true)
+
+//Using VAR, LET, CONST
+
+//Area of a Circle
+console.log("Area of a Circle")
+var radius=5
+console.log(3.14*radius*radius)
+var radius=2
+console.log(3.14*radius*radius)
+
+//Circumference of a Circle
+console.log("Circumference of a Circle")
+let rad=2
+console.log(2*3.14*rad)
+var radi=5
+console.log(2*3.14*radi)
+let a=5
+console.log(2*3.14*a)
+
+//Radius of the Circle
+console.log("Radius of the Circle")
+const c=5
+console.log("Radius of the circle is: " + c)
+var b=4
+console.log(b)
+let o=9
+console.log(o)
